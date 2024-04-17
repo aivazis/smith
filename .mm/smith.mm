f9238e4e36713ac041b570b41d2700a5e40fcfc3
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-17 08:40:17 -0700

    mm: primed the build

---
+++ .mm/smith.mm
@@ -0,0 +1,22 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# smith consists of a python package
+smith.packages := smith.pkg
+# a ux bundle
+smith.webpack := smith.ux
+# and tests
+smith.tests := smith.pkg.tests
+
+# load the packages
+include $(smith.packages)
+# the ux
+include $(smith.webpack)
+# the test suites
+include $(smith.tests)
+
+
+# end of file
