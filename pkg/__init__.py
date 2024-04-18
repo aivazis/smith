aa355933dea8ae8b4b4368eb5a28353d08fb5cad
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-17 17:02:10 -0700

    smith: published the app

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,7 +1,102 @@
+# -*- python -*-
 # -*- coding: utf-8 -*-
 #
 # michael a.g. aïvázis <michael.aivazis@para-sim.com>
 # (c) 1998-2024 all rights reserved
+
+
+# import and publish pyre symbols
+from pyre import (
+    # protocols, components, traits, and their infrastructure
+    schemata,
+    constraints,
+    properties,
+    protocol,
+    component,
+    foundry,
+    # decorators
+    export,
+    provides,
+    # the manager of the pyre runtime
+    executive,
+    # support for concurrency
+    nexus,
+    # support for workflows, products, and factories
+    flow,
+    # shells
+    application,
+    plexus,
+    # miscellaneous
+    filesystem,
+    patterns,
+    primitives,
+    tracking,
+    units,
+    timers,
+    weaver,
+    xml,
+    # hdf5 support
+    h5,
+    # bindings of things
+    libpyre,
+)
+
+# register the package with the framework
+package = executive.registerPackage(name="smith", file=__file__)
+# save the geography
+home, prefix, defaults = package.layout()
+
+
+# publish the local modules
+# basic functionality
+from . import meta  # package meta-data
+
+# schema
+from . import gql
+
+# user interfaces
+from . import shells  # the supported application shells
+from . import ux  # interactive user interfaces
+
+
+# administrivia
+def copyright():
+    """
+    Return the copyright note
+    """
+    # pull and print the meta-data
+    return print(meta.header)
+
+
+def license():
+    """
+    Print the license
+    """
+    # pull and print the meta-data
+    return print(meta.license)
+
+
+def built():
+    """
+    Return the build timestamp
+    """
+    # pull and return the meta-data
+    return meta.date
+
+
+def credits():
+    """
+    Print the acknowledgments
+    """
+    return print(meta.acknowledgments)
+
+
+def version():
+    """
+    Return the version
+    """
+    # pull and return the meta-data
+    return meta.version
 
 
 # end of file
