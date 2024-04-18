c1230b00299d2cf31a819ec3184fb6352a5fad02
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-17 20:43:59 -0700

    ux/client: repaired a typo in {boundary}

--- ux/client/boundary.tsx
+++ ux/client/boundary.tsx
@@ -23,7 +23,7 @@ export class ErrorBoundary extends React.Component<Props_t, State_t> {
         clean: true
     }
 
-    public static getDerivedStateFromError(_: Error): State {
+    public static getDerivedStateFromError(_: Error): State_t {
         // Update state so the next render will show the fallback UI.
         return { clean: false };
     }
