a0fff7a21978c2962c9b9d0958833c8cd6c255e1
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-22 01:57:25 +0300

    ux/widgets: primed the {server}

---
+++ ux/client/widgets/server/index.tsx
@@ -0,0 +1,10 @@
+// -*- typescript -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+export { Server } from './server'
+
+
+// end of file
---
+++ ux/client/widgets/server/server.tsx
@@ -0,0 +1,20 @@
+// -*- typescript -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// external
+import React from "react"
+
+
+// server
+export const Server = () => {
+    // render
+    return (
+        <span>hello</span>
+    )
+}
+
+
+// end of file
